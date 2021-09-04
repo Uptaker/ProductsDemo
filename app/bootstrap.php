02900463f777cad7620dec47945376beb51667ed
Uptaker <markus.tammeoja@gmail.com>
2021-09-04 19:17:10 +0300

    Added files to bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,15 +3,17 @@
 // Load config
 require_once('config/config.php');
 
-// Load the libraries, replaced by autoload (see below)
-// require_once('libraries/Core.php');
-// require_once('libraries/Controller.php');
-// require_once('libraries/Database.php');
+// Core
+require_once('libraries/Core.php');
+require_once('libraries/Controller.php');
+require_once('libraries/Database.php');
 
-// Autoload core libraries
-spl_autoload_register(function ($className) {
-    require_once('libraries/' . $className . '.php');
-});
+// Models
+require_once('models/Product.php');
+require_once('models/Dvd.php');
+require_once('models/Book.php');
+require_once('models/Furniture.php');
 
-// Load Helpers
+// Helpers
 require_once('helpers/url_helper.php');
+require_once('helpers/session_helper.php');
